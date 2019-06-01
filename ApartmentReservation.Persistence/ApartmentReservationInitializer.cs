ff6cefa8ce001bc48642d69539bec4a078e702b7
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-01 13:47:48 +0200

    Added Seed methods for Users, Admins, Hosts and Guests

--- ApartmentReservation.Persistence/ApartmentReservationInitializer.cs
+++ ApartmentReservation.Persistence/ApartmentReservationInitializer.cs
@@ -1,19 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using ApartmentReservation.Application.Infrastructure.Authentication;
+using ApartmentReservation.Domain.Entities;
 
 namespace ApartmentReservation.Persistence
 {
     public class ApartmentReservationInitializer
     {
+        private readonly Dictionary<long, Administrator> Administrators = new Dictionary<long, Administrator>();
+        private readonly Dictionary<long, Guest> Guests = new Dictionary<long, Guest>();
+        private readonly Dictionary<long, Host> Hosts = new Dictionary<long, Host>();
+        private readonly Dictionary<long, User> Users = new Dictionary<long, User>();
+
         public static void Initialize(ApartmentReservationDbContext context)
         {
             var initializer = new ApartmentReservationInitializer();
             initializer.SeedEverything(context);
         }
 
-        private void SeedEverything(ApartmentReservationDbContext context)
+        protected void SeedEverything(ApartmentReservationDbContext context)
         {
             context.Database.EnsureCreated();
 
@@ -21,6 +27,75 @@ namespace ApartmentReservation.Persistence
             {
                 return; // Db is seeded
             }
+
+            this.SeedUsers(context);
+            this.SeedAdministrators(context);
+            this.SeedHosts(context);
+            this.SeedGuests(context);
+        }
+
+        private void SeedAdministrators(ApartmentReservationDbContext context)
+        {
+            context.Administrators.AddRange(this.Users.Values
+                .Where(u => u.RoleName == RoleNames.Administrator)
+                .Select(u => new Administrator()
+                {
+                    User = u
+                }));
+
+            context.SaveChanges();
+        }
+
+        private void SeedGuests(ApartmentReservationDbContext context)
+        {
+            context.Guests.AddRange(this.Users.Values
+                .Where(u => u.RoleName == RoleNames.Guest)
+                .Select(u => new Guest()
+                {
+                    User = u
+                }));
+
+            context.SaveChanges();
+        }
+
+        private void SeedHosts(ApartmentReservationDbContext context)
+        {
+            context.Hosts.AddRange(this.Users.Values
+                .Where(u => u.RoleName == RoleNames.Host)
+                .Select(u => new Host()
+                {
+                    User = u
+                }));
+
+            context.SaveChanges();
+        }
+
+        private void SeedUsers(ApartmentReservationDbContext context)
+        {
+            this.Users[1] = new User()
+            {
+                Username = "admin",
+                Password = "admin",
+                RoleName = RoleNames.Administrator
+            };
+
+            this.Users[2] = new User()
+            {
+                Username = "host",
+                Password = "host",
+                RoleName = RoleNames.Host
+            };
+
+            this.Users[3] = new User()
+            {
+                Username = "guest",
+                Password = "guest",
+                RoleName = RoleNames.Guest
+            };
+
+            context.Users.AddRange(this.Users.Values);
+
+            context.SaveChanges();
         }
     }
 }
