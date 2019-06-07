6a9cee37b094f414c982c7377623d056b9e441c2
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-07 11:45:07 +0200

    Added Users controller and tests for it

---
+++ ApartmentReservation.WebUI.UnitTests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ApartmentReservation.Application.Dtos;
+using ApartmentReservation.Application.Features.Users.Queries;
+using ApartmentReservation.WebUI.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace ApartmentReservation.WebUI.UnitTests.Controllers
+{
+    public class UsersControllerTests
+    {
+        [Fact]
+        public async Task GetAll_WhenInvoked_CallsMediator()
+        {
+            var expectedValueResult = new List<UserDto> { new UserDto() { Username = "guest", Password = "guest" } };
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock.Setup(m => m.Send(It.IsAny<GetAllUsersQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(expectedValueResult);
+
+            var controller = new UsersController(mediatorMock.Object);
+
+            var result = await controller.Get().ConfigureAwait(false);
+
+            var okResult = Assert.IsAssignableFrom<OkObjectResult>(result);
+            var value = Assert.IsAssignableFrom<IEnumerable<UserDto>>(okResult.Value);
+            Assert.Equal(expectedValueResult, value);
+            mediatorMock.Verify(m => m.Send(It.IsAny<GetAllUsersQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
---
+++ ApartmentReservation.WebUI/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using ApartmentReservation.Application.Features.Users.Queries;
+using ApartmentReservation.Application.Infrastructure.Authentication;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentReservation.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [Authorize(Policy = Policies.AdministratorOnly)]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok(await this.mediator.Send(new GetAllUsersQuery()).ConfigureAwait(false));
+        }
+    }
+}
