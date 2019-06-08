458919d21c0a3c0088e83fa950db9f5dafdfd992
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-08 23:03:16 +0200

    added GetUserQueryHandler test and made it past

---
+++ ApartmentReservation.Application/Features/Users/Queries/GetUserQuery.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ApartmentReservation.Application.Dtos;
+using ApartmentReservation.Application.Exceptions;
+using ApartmentReservation.Application.Interfaces;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApartmentReservation.Application.Features.Users.Queries
+{
+    public class GetUserQuery : IRequest<UserDto>
+    {
+        public long Id { get; set; }
+    }
+
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserDto>
+    {
+        private readonly IApartmentReservationDbContext context;
+        private readonly IMapper mapper;
+
+        public GetUserQueryHandler(IApartmentReservationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<UserDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var dbUser = await this.context.Users.SingleOrDefaultAsync(u => u.Id == request.Id && !u.IsDeleted).ConfigureAwait(false);
+
+            if (dbUser is null)
+            {
+                throw new NotFoundException($"User with id={request.Id} could not be found!");
+            }
+
+            return this.mapper.Map<UserDto>(dbUser);
+        }
+    }
+}
