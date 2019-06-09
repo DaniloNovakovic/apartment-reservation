6f7a8813df31a1a93ca20afee0c7b21ee826f507
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-09 17:20:35 +0200

    Added Amenities controller

---
+++ ApartmentReservation.WebUI/Controllers/AmenitiesController.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using ApartmentReservation.Application.Features.Amenities.Commands;
+using ApartmentReservation.Application.Features.Amenities.Queries;
+using ApartmentReservation.Application.Infrastructure.Authentication;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentReservation.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = Policies.AdministratorOrHostOnly)]
+    public class AmenitiesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public AmenitiesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        // GET: api/Amenities
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return this.Ok(await this.mediator.Send(new GetAllAmenitiesQuery()).ConfigureAwait(false));
+        }
+
+        // GET: api/Amenities/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(long id)
+        {
+            return this.Ok(await this.mediator.Send(new GetAmenityQuery() { Id = id }).ConfigureAwait(false));
+        }
+
+        // POST: api/Amenities
+        [HttpPost]
+        [Authorize(Policy = Policies.AdministratorOnly)]
+        public async Task<IActionResult> Post([FromBody] CreateAmenityCommand command)
+        {
+            await this.mediator.Send(command).ConfigureAwait(false);
+
+            return this.NoContent();
+        }
+
+        // PUT: api/Amenities/5
+        [HttpPut("{id}")]
+        [Authorize(Policy = Policies.AdministratorOnly)]
+        public async Task<IActionResult> Put(long id, [FromBody] UpdateAmenityCommand command)
+        {
+            command.Id = id;
+            await this.mediator.Send(command).ConfigureAwait(false);
+
+            return this.NoContent();
+        }
+
+        // DELETE: api/Amenities/5
+        [HttpDelete("{id}")]
+        [Authorize(Policy = Policies.AdministratorOnly)]
+        public async Task<IActionResult> Delete(long id)
+        {
+            await this.mediator.Send(new DeleteAmenityCommand() { Id = id }).ConfigureAwait(false);
+
+            return this.NoContent();
+        }
+    }
+}
