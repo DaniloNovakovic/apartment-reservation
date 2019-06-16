d545031126d30e11b114e3759caeb8e961a5cc21
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-16 22:01:50 +0200

    Finished writing UpdateApartmentCommand feature

---
+++ ApartmentReservation.Application.IntegrationTests/Features/Apartments/Commands/UpdateApartmentCommandHandlerTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ApartmentReservation.Application.Features.Apartments.Commands;
+using ApartmentReservation.Domain.Constants;
+using ApartmentReservation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ApartmentReservation.Application.IntegrationTests.Features.Apartments.Commands
+{
+    public class UpdateApartmentCommandHandlerTests : InMemoryContextTestBase
+    {
+        private readonly UpdateApartmentCommandHandler sut;
+        private Apartment dbApartment;
+
+        public UpdateApartmentCommandHandlerTests()
+        {
+            this.sut = new UpdateApartmentCommandHandler(this.Context);
+        }
+
+        [Fact]
+        public async Task UpdatesApartment()
+        {
+            var request = new UpdateApartmentCommand()
+            {
+                Id = dbApartment.Id,
+                ActivityState = ActivityStates.Inactive,
+                ApartmentType = ApartmentTypes.SingleRoom,
+                CheckInTime = "11:00:00",
+                CheckOutTime = "20:15:33",
+                Title = "My new title",
+                Latitude = 33,
+                Longitude = 34,
+                CityName = "New York",
+                StreetName = "Digit Street",
+                StreetNumber = "33",
+                CountryName = "America"
+            };
+
+            await sut.Handle(request, CancellationToken.None).ConfigureAwait(false);
+
+            dbApartment = await this.Context.Apartments
+                .Include(a => a.Location)
+                .ThenInclude(l => l.Address)
+                .SingleOrDefaultAsync(a => a.Id == request.Id && !a.IsDeleted)
+                .ConfigureAwait(false);
+
+            Assert.NotNull(dbApartment);
+            Assert.Equal(dbApartment.Title, request.Title);
+            Assert.NotNull(dbApartment.Location);
+            Assert.Equal(dbApartment.Location.Latitude, request.Latitude);
+            Assert.Equal(dbApartment.Location.Longitude, request.Longitude);
+            Assert.NotNull(dbApartment.Location.Address);
+            Assert.Equal(dbApartment.Location.Address.CountryName, request.CountryName);
+        }
+
+        protected override void LoadTestData()
+        {
+            this.dbApartment = this.Context.Add(new Apartment()
+            {
+                ActivityState = ActivityStates.Active,
+                CheckInTime = "14:00:00",
+                CheckOutTime = "10:00:00",
+                Title = "My Title",
+                ApartmentType = ApartmentTypes.Full,
+                PricePerNight = 32,
+                NumberOfRooms = 3,
+                Location = new Location()
+                {
+                    Latitude = 2,
+                    Longitude = 3,
+                    Address = new Address()
+                    {
+                        CityName = "Novi Sad",
+                        StreetName = "Djura lazevica",
+                        StreetNumber = "25"
+                    }
+                }
+            }).Entity;
+
+            this.Context.SaveChanges();
+        }
+    }
+}
---
+++ ApartmentReservation.Application/Features/Apartments/Commands/UpdateApartmentCommand.cs
@@ -0,0 +1,123 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ApartmentReservation.Application.Exceptions;
+using ApartmentReservation.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ApartmentReservation.Domain.Entities;
+
+namespace ApartmentReservation.Application.Features.Apartments.Commands
+{
+    public class UpdateApartmentCommand : IRequest
+    {
+        public long Id { get; set; }
+        public string ApartmentType { get; set; }
+        public string ActivityState { get; set; }
+        public string CheckInTime { get; set; }
+        public string CheckOutTime { get; set; }
+        public string CityName { get; set; }
+        public string CountryName { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public int? NumberOfRooms { get; set; }
+        public int? NumberOfGuests { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public double? PricePerNight { get; set; }
+        public string StreetName { get; set; }
+
+        public string StreetNumber { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class UpdateApartmentCommandHandler : IRequestHandler<UpdateApartmentCommand>
+    {
+        private readonly IApartmentReservationDbContext context;
+
+        public UpdateApartmentCommandHandler(IApartmentReservationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateApartmentCommand request, CancellationToken cancellationToken)
+        {
+            var dbApartment = await this.context.Apartments
+                .Include(a => a.Location)
+                .ThenInclude(l => l.Address)
+                .SingleOrDefaultAsync(a => a.Id == request.Id && !a.IsDeleted)
+                .ConfigureAwait(false);
+
+            if (dbApartment is null)
+            {
+                throw new NotFoundException();
+            }
+
+            if (dbApartment.Location is null)
+            {
+                dbApartment.Location = new Location();
+            }
+            if (dbApartment.Location.Address is null)
+            {
+                dbApartment.Location.Address = new Address();
+            }
+
+            MapValues(request, dbApartment);
+
+            await context.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+            return Unit.Value;
+        }
+
+        private static void MapValues(UpdateApartmentCommand request, Apartment dbApartment)
+        {
+            if (!string.IsNullOrEmpty(request.ActivityState))
+                dbApartment.ActivityState = request.ActivityState;
+
+            if (!string.IsNullOrEmpty(request.ApartmentType))
+                dbApartment.ApartmentType = request.ApartmentType;
+
+            if (!string.IsNullOrEmpty(request.Title))
+                dbApartment.Title = request.Title;
+
+            if (!string.IsNullOrEmpty(request.CheckInTime))
+                dbApartment.CheckInTime = request.CheckInTime;
+
+            if (!string.IsNullOrEmpty(request.CheckOutTime))
+                dbApartment.CheckOutTime = request.CheckOutTime;
+
+            dbApartment.NumberOfGuests = request.NumberOfGuests ?? dbApartment.NumberOfGuests;
+            dbApartment.NumberOfRooms = request.NumberOfRooms ?? dbApartment.NumberOfRooms;
+
+            var location = dbApartment.Location;
+            MapLocation(request, location);
+        }
+
+        private static void MapLocation(UpdateApartmentCommand request, Location location)
+        {
+            location.Latitude = request.Latitude ?? location.Latitude;
+            location.Longitude = request.Longitude ?? location.Longitude;
+
+            var address = location.Address;
+            MapAddress(request, address);
+        }
+
+        private static void MapAddress(UpdateApartmentCommand request, Address address)
+        {
+            if (!string.IsNullOrEmpty(request.CityName))
+                address.CityName = request.CityName;
+
+            if (!string.IsNullOrEmpty(request.CountryName))
+                address.CountryName = request.CountryName;
+
+            if (!string.IsNullOrEmpty(request.StreetName))
+                address.StreetName = request.StreetName;
+
+            if (!string.IsNullOrEmpty(request.StreetNumber))
+                address.StreetNumber = request.StreetNumber;
+
+            if (!string.IsNullOrEmpty(request.PostalCode))
+                address.PostalCode = request.PostalCode;
+        }
+    }
+}
