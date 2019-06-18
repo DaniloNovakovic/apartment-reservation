123892de7167a5962eaf99d261f8ec2229b96746
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-18 14:36:33 +0200

    reduced apartmentcard height

--- ApartmentReservation.WebUI/ClientApp/src/components/apartments/ApartmentCard.js
+++ ApartmentReservation.WebUI/ClientApp/src/components/apartments/ApartmentCard.js
@@ -5,63 +5,11 @@ import { Card, Row, Col } from "react-bootstrap";
 import ReactStars from "react-stars";
 import { Link } from "react-router-dom";
 
-const defaultProps = {
-  apartment: {
-    id: 1,
-    activityState: "Active",
-    amenities: [
-      { name: "TV" },
-      { name: "Kitchen" },
-      { name: "Bed" },
-      { name: "Refrigirator" },
-      { name: "Microwave" }
-    ],
-    apartmentType: "Full",
-    title: "Magnificent apartment",
-    checkInTime: null,
-    checkOutTime: null,
-    comments: [],
-    forRentalDates: [],
-    host: null,
-    images: [
-      {
-        uri:
-          "https://www.onni.com/wp-content/uploads/2016/11/Rental-Apartment-Page-new-min.jpg"
-      },
-      {
-        uri:
-          "https://arystudios.files.wordpress.com/2015/08/3dcontemperoryapartmentrenderingarchitecturalduskviewrealisticarystudios.jpg"
-      },
-      {
-        uri:
-          "https://www.travelonline.com/melbourne/city-cbd/accommodation/adina-apartment-hotel-melbourne-flinders-street/penthouse-76880.jpg"
-      }
-    ],
-    location: {
-      id: 0,
-      address: {
-        id: 1,
-        cityName: "Novi Sad",
-        postalCode: "21102",
-        streetName: "Bulevar kralja Petra",
-        streetNumber: "25"
-      },
-      latitude: 45.267136,
-      longitude: 19.833549
-    },
-    numberOfGuests: 0,
-    numberOfRooms: 1,
-    pricePerNight: 35,
-    rating: 3,
-    reservations: []
-  }
-};
-
 export class ApartmentCard extends Component {
   constructor(props) {
     super(props);
 
-    let apartment = props.apartment || defaultProps.apartment;
+    let apartment = props.apartment || { images: [] };
 
     if (!apartment.images || apartment.images.length === 0) {
       apartment.images = [
@@ -103,8 +51,6 @@ export class ApartmentCard extends Component {
                 .map(amenity => amenity.name)
                 .join(" · ")}
             </Card.Text>
-          </Card.Body>
-          <Card.Body>
             <Row>
               <Col>
                 <ReactStars count={5} edit={false} value={apartment.rating} />
