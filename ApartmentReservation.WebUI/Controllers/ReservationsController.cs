cee7bc0ec03301066a4e60c9611b30fb0ae30092
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-19 20:06:04 +0200

    Created controller for reservations

---
+++ ApartmentReservation.WebUI/Controllers/ReservationsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using ApartmentReservation.Application.Features.Reservations.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApartmentReservation.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ReservationsController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] GetAllReservationsQuery query)
+        {
+            return this.Ok(await this.mediator.Send(query).ConfigureAwait(false));
+        }
+    }
+}
