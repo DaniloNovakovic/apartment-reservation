f959b1991f46190e15bb3361775cc738b0e35a91
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-19 19:55:37 +0200

    Added GetAllReservation feature with filters

--- ApartmentReservation.Application/Dtos/ReservationDto.cs
+++ ApartmentReservation.Application/Dtos/ReservationDto.cs
@@ -1,4 +1,6 @@
-﻿using ApartmentReservation.Domain.Entities;
+﻿using System;
+using ApartmentReservation.Domain.Constants;
+using ApartmentReservation.Domain.Entities;
 
 namespace ApartmentReservation.Application.Dtos
 {
@@ -8,8 +10,28 @@ namespace ApartmentReservation.Application.Dtos
         {
         }
 
-        public ReservationDto(Reservation a)
+        public ReservationDto(Reservation reservation)
         {
+            Id = reservation.Id;
+            Apartment = new ApartmentDto(reservation.Apartment);
+            ReservationStartDate = reservation.ReservationStartDate;
+            NumberOfNightsRented = reservation.NumberOfNightsRented;
+            TotalCost = reservation.TotalCost;
+            Guest = new UserPublicDto(reservation.Guest);
+            ReservationState = reservation.ReservationState;
         }
+
+        public long Id { get; set; }
+        public ApartmentDto Apartment { get; set; }
+
+        public DateTime ReservationStartDate { get; set; }
+
+        public int NumberOfNightsRented { get; set; }
+
+        public double TotalCost { get; set; }
+
+        public UserPublicDto Guest { get; set; }
+
+        public string ReservationState { get; set; }
     }
 }
---
+++ ApartmentReservation.Application/Features/Reservations/Queries/GetAllReservationsQuery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ApartmentReservation.Application.Dtos;
+using ApartmentReservation.Application.Interfaces;
+using ApartmentReservation.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApartmentReservation.Application.Features.Reservations.Queries
+{
+    public class GetAllReservationsQuery : IRequest<IEnumerable<ReservationDto>>
+    {
+        public long? HostId { get; set; }
+        public long? GuestId { get; set; }
+    }
+
+    public class GetAllReservationsQueryHandler : IRequestHandler<GetAllReservationsQuery, IEnumerable<ReservationDto>>
+    {
+        private readonly IApartmentReservationDbContext context;
+
+        public GetAllReservationsQueryHandler(IApartmentReservationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<ReservationDto>> Handle(GetAllReservationsQuery request, CancellationToken cancellationToken)
+        {
+            var query = context.Reservations
+                .Include(r => r.Guest).ThenInclude(g => g.User)
+                .Include(r => r.Apartment).ThenInclude(a => a.Host).ThenInclude(h => h.User)
+                .Where(r => !r.IsDeleted && !r.Apartment.IsDeleted && !r.Guest.IsDeleted);
+
+            query = this.ApplyFilters(request, query);
+
+            var reservations = await query.ToListAsync(cancellationToken).ConfigureAwait(false);
+
+            return reservations.Select(r => new ReservationDto(r));
+        }
+
+        private IQueryable<Reservation> ApplyFilters(GetAllReservationsQuery filters, IQueryable<Reservation> query)
+        {
+            if (filters.HostId != null)
+            {
+                query = query.Where(r => r.Apartment.HostId == filters.HostId);
+            }
+
+            if (filters.GuestId != null)
+            {
+                query = query.Where(r => r.GuestId == filters.GuestId);
+            }
+
+            return query;
+        }
+    }
+}
