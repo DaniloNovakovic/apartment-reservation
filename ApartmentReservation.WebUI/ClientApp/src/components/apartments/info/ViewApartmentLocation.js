e8a215eb9bfaa76988e5df51424c71201bfab496
Danilo Novakovic <dakenzi97@gmail.com>
2019-06-27 09:55:55 +0200

    Map is now displayed to the right of the Location info

--- ApartmentReservation.WebUI/ClientApp/src/components/apartments/info/ViewApartmentLocation.js
+++ ApartmentReservation.WebUI/ClientApp/src/components/apartments/info/ViewApartmentLocation.js
@@ -12,30 +12,32 @@ export default function ViewApartmentLocation({
   return (
     <article className="view-apartment-location">
       <h5>Location</h5>
-      <div className="summary-content">
-        <p>
-          <b>Street Name:</b> {address.streetName}
-        </p>
-        <p>
-          <b>Street Number:</b> {address.streetNumber}
-        </p>
-        <p>
-          <b>City:</b> {address.cityName}
-        </p>
-        <p>
-          <b>State:</b> {address.countryName}
-        </p>
-        <p>
-          <b>Zip:</b> {address.postalCode}
-        </p>
-        <p>
-          <b>Longitude:</b> {longitude}
-        </p>
-        <p>
-          <b>Latitude:</b> {latitude}
-        </p>
+      <div className="location-details">
+        <div className="summary-content">
+          <p>
+            <b>Street Name:</b> {address.streetName}
+          </p>
+          <p>
+            <b>Street Number:</b> {address.streetNumber}
+          </p>
+          <p>
+            <b>City:</b> {address.cityName}
+          </p>
+          <p>
+            <b>State:</b> {address.countryName}
+          </p>
+          <p>
+            <b>Zip:</b> {address.postalCode}
+          </p>
+          <p>
+            <b>Longitude:</b> {longitude}
+          </p>
+          <p>
+            <b>Latitude:</b> {latitude}
+          </p>
+        </div>
+        <OpenLayersMap lon={longitude} lat={latitude} readonly />
       </div>
-      <OpenLayersMap lon={longitude} lat={latitude} readonly />
       {allowEdit && (
         <EditApartmentLocationModal
           formData={{ ...address, longitude, latitude }}
