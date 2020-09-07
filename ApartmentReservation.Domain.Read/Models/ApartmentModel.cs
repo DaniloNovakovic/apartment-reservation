6dc945e17ee95faa8c8811e3f0762cfb3084a608
Danilo Novakovic <dakenzi97@gmail.com>
2020-09-07 13:40:14 +0200

    Added QueryModel (domain for read side - nosql)

---
+++ ApartmentReservation.Domain.Read/Models/ApartmentModel.cs
@@ -0,0 +1,28 @@
+﻿using ApartmentReservation.Domain.Read.Common;
+using System;
+using System.Collections.Generic;
+
+namespace ApartmentReservation.Domain.Read.Models
+{
+    public class ApartmentModel : Record
+    {
+        public long? HostId { get; set; }
+
+        public string ActivityState { get; set; }
+        public string ApartmentType { get; set; }
+        public string CheckInTime { get; set; }
+        public string CheckOutTime { get; set; }
+        public LocationModel Location { get; set; }
+        public int NumberOfGuests { get; set; }
+        public int NumberOfRooms { get; set; }
+        public double PricePerNight { get; set; }
+        public string Title { get; set; }
+        public double Rating { get; set; }
+
+        public ICollection<ImageModel> Images { get; set; } = new HashSet<ImageModel>();
+        public ICollection<AmenityModel> Amenities { get; set; } = new HashSet<AmenityModel>();
+        public ICollection<DateTime> ForRentalDates { get; set; } = new HashSet<DateTime>();
+        public ICollection<DateTime> AvailableDates { get; set; } = new HashSet<DateTime>();
+        public ICollection<CommentModel> Comments { get; set; } = new HashSet<CommentModel>();
+    }
+}
