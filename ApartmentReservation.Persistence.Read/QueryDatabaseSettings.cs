7fba89ee732478f4aa8ea7caef7228cbdae0f723
Danilo Novakovic <dakenzi97@gmail.com>
2020-09-07 14:06:11 +0200

    Changed target versions to 2.1, added project refferences and created Persistence.Read for noSQL db

---
+++ ApartmentReservation.Persistence.Read/QueryDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using ApartmentReservation.Application.Interfaces;
+
+namespace ApartmentReservation.Persistence.Read
+{
+
+    public class QueryDatabaseSettings : IQueryDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string UsersCollectionName { get; set; }
+        public string ApartmentsCollectionName { get; set; }
+        public string ReservationsCollectionName { get; set; }
+    }
+}
---
+++ ApartmentReservation.Persistence.Read/QueryDbContext.cs
@@ -0,0 +1,48 @@
+﻿using ApartmentReservation.Application.Interfaces;
+using ApartmentReservation.Domain.Read.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace ApartmentReservation.Persistence.Read
+{
+    public class QueryDbContext : IQueryDbContext
+    {
+        private readonly IMongoCollection<Counter> _counters;
+        private readonly IMongoDatabase _db;
+
+        public QueryDbContext(IQueryDatabaseSettings dbSettings)
+        {
+            var client = new MongoClient(dbSettings.ConnectionString);
+            _db = client.GetDatabase(dbSettings.DatabaseName);
+
+            Users = _db.GetCollection<UserModel>(dbSettings.UsersCollectionName);
+            Apartments = _db.GetCollection<ApartmentModel>(dbSettings.ApartmentsCollectionName);
+            Reservations = _db.GetCollection<ReservationModel>(dbSettings.ReservationsCollectionName);
+            _counters = _db.GetCollection<Counter>("Counters");
+        }
+
+        public IMongoCollection<ApartmentModel> Apartments { get; set; }
+        public IMongoCollection<ReservationModel> Reservations { get; set; }
+        public IMongoCollection<UserModel> Users { get; set; }
+
+        public long GetNextSequence(string collectionName)
+        {
+            Counter ret = _counters.FindOneAndUpdate<Counter>(
+                c => c.Id == collectionName,
+                Builders<Counter>.Update.Inc(c => c.Sequence, 1),
+                new FindOneAndUpdateOptions<Counter, Counter> { IsUpsert = true });
+
+            return ret.Sequence;
+        }
+
+        private class Counter
+        {
+            [BsonId]
+            [BsonRepresentation(BsonType.String)]
+            public string Id { get; set; }
+
+            public long Sequence { get; set; } = 0;
+        }
+    }
+}
